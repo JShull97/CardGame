bb82512a5639f68407e5220990dc68eacc0ead85
JShull97 <38473222+JShull97@users.noreply.github.com>
2018-10-16 16:53:19 -0500

    Create Deck.java
    Created Deck

---
+++ Deck.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author js0899490
+ */
+public class Deck {
+    public static void main(String[] args) {
+        String[] SUITS = {
+            "Clubs", "Diamonds", "Hearts", "Spades"
+        };
+
+        String[] RANKS = {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "King", "Ace"
+        };
+
+        // initialize deck
+        int n = SUITS.length * RANKS.length;
+        String[] deck = new String[n];
+        for (int i = 0; i < RANKS.length; i++) {
+            for (int j = 0; j < SUITS.length; j++) {
+                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+            }
+        }
+
+        // shuffle
+        for (int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n-i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+
+        // print shuffled deck
+        for (int i = 0; i < n; i++) {
+            System.out.println(deck[i]);
+        }
+    }
+
+}
